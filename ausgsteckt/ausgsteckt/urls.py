21fa006bfb6f9afebf534aef8356609eb4965bf2
Florian Schweikert <kelvan@ist-total.org>
2016-10-28 00:23:40 +0200

    Add django-leaflet

--- ausgsteckt/ausgsteckt/urls.py
+++ ausgsteckt/ausgsteckt/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
+    url(r'^map/$', TemplateView.as_view(template_name='base.html')),
     url(r'^admin/', admin.site.urls),
 ]
