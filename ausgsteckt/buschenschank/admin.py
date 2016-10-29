8cace3515944caa72aeb05af7833e22402a91bf6
Florian Schweikert <kelvan@ist-total.org>
2016-10-30 01:44:14 +0200

    Enhance admin
    Show address in admin
    Search in tags

--- ausgsteckt/buschenschank/admin.py
+++ ausgsteckt/buschenschank/admin.py
@@ -5,8 +5,8 @@ from .models import Buschenschank
 
 @admin.register(Buschenschank)
 class BuschenschankAdmin(admin.GeoModelAdmin):
-    list_display = ('name', 'cuisine', 'latitude', 'longitude')
-    search_fields = ('name',)
+    list_display = ('name', 'cuisine', 'latitude', 'longitude', 'address')
+    search_fields = ('name', 'tags')
     
     def cuisine(self, instance):
         return instance.tags.get('cuisine')
