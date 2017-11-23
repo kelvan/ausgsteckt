51d0b81a010693918f4c484a5e83da649da2eb41
Florian Schweikert <kelvan@ist-total.org>
2017-11-23 15:58:39 +0100

    Refactor templates, add accordion to detail page to collapse tags

--- ausgsteckt/buschenschank/templatetags/osm_tags.py
+++ ausgsteckt/buschenschank/templatetags/osm_tags.py
@@ -1,8 +1,36 @@
 from django import template
+from django.template.loader import render_to_string
 
 register = template.Library()
+
+BADGE_TAGS = ['cuisine']
+LIST_TAGS = ['opening_hours']
 
 
 @register.simple_tag
 def osmtag(node, tagname):
     return node.tags.get(tagname)
+
+
+@register.filter
+def format_value_list(value, tagname=None):
+    """
+    convert osm tag value to nice html representation
+    :param value: osm tag value as ';' separated string
+    :return: html
+    """
+    value = value.strip()
+    if not value:
+        return ''
+
+    parts = value.split(';')
+
+    make_badge = False
+    make_list = False
+
+    if tagname:
+        make_badge = tagname in BADGE_TAGS
+        make_list = tagname in LIST_TAGS
+
+    context = {'values': parts, 'make_badge': make_badge, 'make_list': make_list}
+    return render_to_string('buschenschank/includes/tag_value_list.html', context=context)
