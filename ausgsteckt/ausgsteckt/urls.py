9accd36a80c1ca1aba56a37e8e670d191e1149ab
Florian Schweikert <kelvan@ist-total.org>
2017-11-30 15:08:17 +0100

    Add 404/500 templates

--- ausgsteckt/ausgsteckt/urls.py
+++ ausgsteckt/ausgsteckt/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from django.urls import reverse_lazy
 from django.contrib import admin
 from django.views.generic import RedirectView
+from django.views import defaults as default_views
 from django.contrib.flatpages import views as flatpages_views
 
 
@@ -18,4 +19,11 @@ urlpatterns = [
     url(r'^buschenschank/', include('buschenschank.urls')),
     url(r'^osm/', include('data_quality.urls')),
     url(r'^impressum/$', flatpages_views.flatpage, {'url': '/impressum/'}, name='impressum'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [
+        url(r'^404/$', default_views.page_not_found, kwargs={'exception': Exception('Page not Found')}),
+        url(r'^500/$', default_views.server_error),
+    ]
