fb183432d596f8b98bda09444c6989b7c8450afd
Florian Schweikert <kelvan@ist-total.org>
2018-01-30 13:49:42 +0100

    Add links to city filter in fixme view

--- ausgsteckt/data_quality/templatetags/data_quality.py
+++ ausgsteckt/data_quality/templatetags/data_quality.py
@@ -1,4 +1,6 @@
 from django import template
+from django.utils.html import format_html, mark_safe
+from django.urls import reverse
 
 register = template.Library()
 
@@ -54,3 +56,25 @@ def contact_coverage(tags, percentage=False):
     if percentage:
         return round(coverage*100, 1)
     return round(coverage, 3)
+
+
+@register.filter
+def fixme_address(item):
+    if item.street or item.housenumber or item.postcode or item.city:
+        if item.city:
+            city = format_html("<a href={url}>{city}</a>",
+                        city=item.city,
+                        url=reverse('data_quality:fixme_buschenschank', kwargs={'cityname': item.city})
+                        )
+        else:
+            city = '<city unknown>'
+
+        addr = format_html('{street} {number}, {postcode} {city}',
+            street=(item.street or item.place or '<street unknown>'),
+            number=(item.housenumber or '<number unknown>'),
+            postcode=(item.postcode or '<postcode unknown>'),
+            city=city
+        )
+        if item.country:
+            addr += mark_safe(', ' + item.country)
+        return addr
