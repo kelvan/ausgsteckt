80e5c25b9c33ae2a72bee3979c40aae5472a43b9
Florian Schweikert <kelvan@ist-total.org>
2018-05-25 14:25:17 +0200

    Remove useless get_context_data override

--- ausgsteckt/buschenschank/views.py
+++ ausgsteckt/buschenschank/views.py
@@ -49,11 +49,6 @@ class BuschenschankDetailView(PageTitleMixin, DetailView):
 class PublicBuschenschankGeoJsonView(ListView):
     model = Buschenschank
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['page_title']
-        return context
-
     def get_queryset(self):
         queryset = super().get_queryset()
         return queryset.filter(is_removed=False)
